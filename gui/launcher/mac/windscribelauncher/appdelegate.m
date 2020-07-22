fba29ad069fe2a3b512024e72ed065ae6850ba33
Alexey Azanov <alexey@windscribe.com>
2020-07-22 23:06:56 +0700

    gui/launcher/mac added.

---
+++ gui/launcher/mac/windscribelauncher/appdelegate.m
@@ -0,0 +1,40 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    
+    NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
+    NSArray *apps = [workspace runningApplications];
+    
+    bool isRunning = false;
+    for (NSRunningApplication *a in apps) {
+        if ([a.bundleIdentifier isEqualToString:@"com.windscribe.WindscribeGUI"]) {
+            isRunning = true;
+            break;
+        }
+    }
+    
+    if (!isRunning)
+    {
+        NSString *bundlePath = [[NSBundle mainBundle] bundlePath];
+        for (int i = 0; i < 4; ++i)
+        {
+            bundlePath = [bundlePath stringByDeletingLastPathComponent];
+        }
+        
+        [workspace launchApplication:bundlePath];
+    }
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+
+}
+
+
+@end
