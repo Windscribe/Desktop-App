6815772b89c9f6078c170717dcb4db57071427cf
Alexey Azanov <alexey@windscribe.com>
2020-07-30 03:01:04 +0700

    Wintun driver renamed and updated to version 0.8.1. Added scripts for signing. #152

--- common/prepare_build_environment/windows/install_openvpn_2_5_0.bat
+++ common/prepare_build_environment/windows/install_openvpn_2_5_0.bat
@@ -6,21 +6,24 @@ rd /s /q "c:\openvpn_temp"
 rd /s /q "c:\libs\openvpn_2_5_0"
 
 mkdir c:\openvpn_temp
-curl.exe https://github.com/OpenVPN/openvpn/archive/master.zip -o c:\openvpn_temp\openvpn-2.5.0.zip -k -L
+curl.exe https://github.com/OpenVPN/openvpn/archive/a7d6977e6e14c512c89e51886b235af153cd4841.zip -o c:\openvpn_temp\openvpn-2.5.0.zip -k -L
 7z x c:\openvpn_temp\openvpn-2.5.0.zip -oc:\openvpn_temp
 
-copy /Y openvpn-2.5.0_changed_files\version.m4 C:\openvpn_temp\openvpn-master\version.m4
-copy /Y openvpn-2.5.0_changed_files\msvc-build.bat C:\openvpn_temp\openvpn-master\msvc-build.bat
-copy /Y openvpn-2.5.0_changed_files\msvc-env.bat C:\openvpn_temp\openvpn-master\msvc-env.bat
-copy /Y openvpn-2.5.0_changed_files\openvpn.sln C:\openvpn_temp\openvpn-master\openvpn.sln
-copy /Y openvpn-2.5.0_changed_files\propertysheet.props C:\openvpn_temp\openvpn-master\src\compat\propertysheet.props
-copy /Y openvpn-2.5.0_changed_files\compat.vcxproj C:\openvpn_temp\openvpn-master\src\compat\compat.vcxproj
-copy /Y openvpn-2.5.0_changed_files\openvpn.vcxproj C:\openvpn_temp\openvpn-master\src\openvpn\openvpn.vcxproj
-copy /Y openvpn-2.5.0_changed_files\tap-windows.h C:\openvpn_temp\openvpn-master\tap-windows.h
-copy /Y openvpn-2.5.0_changed_files\config-msvc.h C:\openvpn_temp\openvpn-master\config-msvc.h
+set openvpn_folder="openvpn-a7d6977e6e14c512c89e51886b235af153cd4841"
+
+copy /Y openvpn-2.5.0_changed_files\version.m4 C:\openvpn_temp\%openvpn_folder%\version.m4
+copy /Y openvpn-2.5.0_changed_files\msvc-build.bat C:\openvpn_temp\%openvpn_folder%\msvc-build.bat
+copy /Y openvpn-2.5.0_changed_files\msvc-env.bat C:\openvpn_temp\%openvpn_folder%\msvc-env.bat
+copy /Y openvpn-2.5.0_changed_files\openvpn.sln C:\openvpn_temp\%openvpn_folder%\openvpn.sln
+copy /Y openvpn-2.5.0_changed_files\propertysheet.props C:\openvpn_temp\%openvpn_folder%\src\compat\propertysheet.props
+copy /Y openvpn-2.5.0_changed_files\compat.vcxproj C:\openvpn_temp\%openvpn_folder%\src\compat\compat.vcxproj
+copy /Y openvpn-2.5.0_changed_files\openvpn.vcxproj C:\openvpn_temp\%openvpn_folder%\src\openvpn\openvpn.vcxproj
+copy /Y openvpn-2.5.0_changed_files\tap-windows.h C:\openvpn_temp\%openvpn_folder%\tap-windows.h
+copy /Y openvpn-2.5.0_changed_files\config-msvc.h C:\openvpn_temp\%openvpn_folder%\config-msvc.h
+copy /Y openvpn-2.5.0_changed_files\tun.h C:\openvpn_temp\%openvpn_folder%\src\openvpn\tun.h
 
 
-PUSHD c:\openvpn_temp\openvpn-master
+PUSHD c:\openvpn_temp\%openvpn_folder%
 
 call msvc-build.bat
 
