c81f24602d01e3c1151c3cb033e788c7448d74ff
Alexander Popov <alexander.popov@windscribe.com>
2020-10-30 15:28:03 -0400

    Fix ip-address related split tunneling problem on Windows. ws/client/desktop/client-desktop#244

--- backend/windows/windscribe_service/split_tunneling/ip_address_table.cpp
+++ backend/windows/windscribe_service/split_tunneling/ip_address_table.cpp
@@ -23,19 +23,29 @@ IpAddressTable::IpAddressTable()
 		pIPAddrTable_ = NULL;
 	}
 
-
-	
+    if (GetUnicastIpAddressTable(AF_INET, &pUnicastIPAddrTable_) != NO_ERROR)
+        pUnicastIPAddrTable_ = nullptr;
 }
 
-
-DWORD IpAddressTable::getAdapterIpAddress(NET_IFINDEX index)
+IpAddressTable::~IpAddressTable()
 {
-	for (DWORD i = 0; i < pIPAddrTable_->dwNumEntries; i++)
-	{
-		if (pIPAddrTable_->table[i].dwIndex == index)
-		{
-			return pIPAddrTable_->table[i].dwAddr;
-		}
-	}
-	return 0;
+    if (pUnicastIPAddrTable_)
+        FreeMibTable(pUnicastIPAddrTable_);
+}
+
+DWORD IpAddressTable::getAdapterIpAddress(NET_IFINDEX index) const
+{
+    // Search in a global IP address table.
+    for (DWORD i = 0; i < pIPAddrTable_->dwNumEntries; ++i) {
+        if (pIPAddrTable_->table[i].dwIndex == index)
+            return pIPAddrTable_->table[i].dwAddr;
+    }
+    if (pUnicastIPAddrTable_) {
+        // Search in a unicast IP address table.
+        for (ULONG i = 0; i < pUnicastIPAddrTable_->NumEntries; ++i) {
+            if (pUnicastIPAddrTable_->Table[i].InterfaceIndex == index)
+                return pUnicastIPAddrTable_->Table[i].Address.Ipv4.sin_addr.S_un.S_addr;
+        }
+    }
+    return 0;
 }
