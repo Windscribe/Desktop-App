24ab5f1aed2094506731ff7c04b82dc7eb3b71fa
Alexey <alexey@windscribe.com>
2020-12-17 02:08:56 +0700

    2.0 build 42.

--- common/version/windscribe_version.h
+++ common/version/windscribe_version.h
@@ -3,8 +3,7 @@
 
 #define WINDSCRIBE_MAJOR_VERSION  2
 #define WINDSCRIBE_MINOR_VERSION 0
-#define WINDSCRIBE_BUILD_VERSION 41
-
+#define WINDSCRIBE_BUILD_VERSION 42
 // only one of these should be enabled; neither -> stable
 #define WINDSCRIBE_IS_BETA
 // #define WINDSCRIBE_IS_GUINEA_PIG
