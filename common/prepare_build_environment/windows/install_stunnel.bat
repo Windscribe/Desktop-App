7830afcd2a9f7a4a6e3f90fbd648ef69e26ec818
WIN-3J6337CRAAE\azano <alexey@windscribe.com>
2020-12-21 19:57:04 +0700

    Stunnel updated to 5.57 (Windows).

--- common/prepare_build_environment/windows/install_stunnel.bat
+++ common/prepare_build_environment/windows/install_stunnel.bat
@@ -5,20 +5,20 @@ rd /s /q "c:\stunnel_temp"
 rd /s /q "c:\libs\stunnel"
 
 mkdir c:\stunnel_temp
-curl.exe https://www.stunnel.org/downloads/stunnel-5.56.tar.gz -o c:\stunnel_temp\stunnel-5.56.tar.gz -k -L
+curl.exe https://www.stunnel.org/downloads/stunnel-5.57.tar.gz -o c:\stunnel_temp\stunnel-5.57.tar.gz -k -L
 
-7z e c:\stunnel_temp\stunnel-5.56.tar.gz -oc:\stunnel_temp
-7z x c:\stunnel_temp\stunnel-5.56.tar -oc:\stunnel_temp
+7z e c:\stunnel_temp\stunnel-5.57.tar.gz -oc:\stunnel_temp
+7z x c:\stunnel_temp\stunnel-5.57.tar -oc:\stunnel_temp
 
-copy /Y stunnel_build_file\makew32.bat c:\stunnel_temp\stunnel-5.56\src\makew32.bat
-copy /Y stunnel_build_file\vc.mak c:\stunnel_temp\stunnel-5.56\src\vc.mak
+copy /Y stunnel_build_file\makew32.bat c:\stunnel_temp\stunnel-5.57\src\makew32.bat
+copy /Y stunnel_build_file\vc.mak c:\stunnel_temp\stunnel-5.57\src\vc.mak
 
-PUSHD c:\stunnel_temp\stunnel-5.56\src
-call c:\stunnel_temp\stunnel-5.56\src\makew32.bat
+PUSHD c:\stunnel_temp\stunnel-5.57\src
+call c:\stunnel_temp\stunnel-5.57\src\makew32.bat
 
 
 mkdir c:\libs\stunnel
-copy c:\stunnel_temp\stunnel-5.56\bin\win32\tstunnel.exe c:\libs\stunnel\tstunnel.exe
+copy c:\stunnel_temp\stunnel-5.57\bin\win32\tstunnel.exe c:\libs\stunnel\tstunnel.exe
 
 POPD
 rd /s /q "c:\stunnel_temp"
