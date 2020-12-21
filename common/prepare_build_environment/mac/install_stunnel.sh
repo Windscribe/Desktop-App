4e0cb68ad2dbd56c74c33a44a89e6b00044202a7
Alexey Azanov <alexey@windscribe.com>
2020-12-22 02:56:30 +0700

    Stunnel updated to 5.57 (Mac).

--- common/prepare_build_environment/mac/install_stunnel.sh
+++ common/prepare_build_environment/mac/install_stunnel.sh
@@ -6,10 +6,10 @@ rm -rf $HOME/"stunnel_temp"
 rm -rf $HOME/"LibsWindscribe/stunnel"
 
 mkdir $HOME/"stunnel_temp"
-curl https://www.stunnel.org/downloads/stunnel-5.56.tar.gz -o $HOME/"stunnel_temp/stunnel-5.56.tar.gz" -k -L
-tar -zxvf $HOME/"stunnel_temp/stunnel-5.56.tar.gz" -C $HOME/"stunnel_temp"
+curl https://www.stunnel.org/downloads/stunnel-5.57.tar.gz -o $HOME/"stunnel_temp/stunnel-5.57.tar.gz" -k -L
+tar -zxvf $HOME/"stunnel_temp/stunnel-5.57.tar.gz" -C $HOME/"stunnel_temp"
 
-pushd $HOME/"stunnel_temp/stunnel-5.56"
+pushd $HOME/"stunnel_temp/stunnel-5.57"
 export CC="cc -mmacosx-version-min=10.11"
 ./configure --prefix=$HOME/"LibsWindscribe/stunnel" --prefix=$HOME/"LibsWindscribe/stunnel" --with-ssl=$HOME/"LibsWindscribe/openssl"
 make
