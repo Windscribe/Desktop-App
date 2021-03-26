a715793ded027dd0c62b7d43b62b0ce32ba65e44
Alexander Popov <alexander.popov@windscribe.com>
2021-03-26 11:05:07 -0400

    Mac building script. ws/client/desktop/client-desktop#435

--- common/ipc/proto/generate_proto.sh
+++ common/ipc/proto/generate_proto.sh
@@ -4,4 +4,8 @@ CUR_PATH=$(pwd)
 
 rm -rf "$CUR_PATH/../generated_proto"
 mkdir -p ../generated_proto
-$HOME/LibsWindscribe/protobuf/bin/protoc -I=$CUR_PATH --cpp_out=$CUR_PATH/../generated_proto $CUR_PATH/*.proto
+
+protodir=$1
+[ -z "$protodir" ] && protodir=$HOME/LibsWindscribe/protobuf/bin
+
+$protodir/protoc -I=$CUR_PATH --cpp_out=$CUR_PATH/../generated_proto $CUR_PATH/*.proto
