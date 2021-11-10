b6a969c5eac9e6c89ad96317d70c10ce9e0ee8b1
Alexey Azanov <alexey@windscribe.com>
2021-11-10 17:44:10 +0700

    Removed unused files from common. #581

--- common/prepare_build_environment/mac/install_autotools.sh
+++
@@ -1,3 +0,0 @@
-#instead install_autotools.sh use commands (works on Catalina):
-#1) brew install libtool
-#2) brew install automake
--- common/prepare_build_environment/windows/stunnel_build_file/makew32.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-TITLE W32 STUNNEL 
-
-set NEWTGTCPU=W32
-
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-
-nmake.exe -f vc.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
