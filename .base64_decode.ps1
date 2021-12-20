409e2cc3eaca9a564ae6ec00aecea257ead7ac52
Ed (Win-bootcamp) <ed@windscribe.com>
2021-12-20 16:06:32 -0500

    Removed toplevel CI and Windows stuff

--- .base64_decode.ps1
+++
@@ -1,1 +0,0 @@
-& {$outpath = ($Env:CODE_SIGNING_PFX_PATH); $inpath = ($Env:CODE_SIGNING_PFX_BASE64); [IO.File]::WriteAllBytes($outpath, ([convert]::FromBase64String(([IO.File]::ReadAllText($inpath)))))}
