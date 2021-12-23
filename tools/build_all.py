79c1d88e3be2973c8fd039ef82468faf07e81a6d
Jonathan Martin <jonathan@windscribe.com>
2021-12-23 12:59:47 -0800

    Replace DropDMG with dmgbuild. #591

--- tools/build_all.py
+++ tools/build_all.py
@@ -601,13 +601,15 @@ def BuildInstallerMac(configdata, qt_root):
     msg.Print("Notarizing...")
     notarize_script = os.path.join(TOOLS_DIR, "notarize.sh")
     iutl.RunCommand([notarize_script, CI_MODE_FLAG])
-  # Drop DMG.
+  # Prepare the disk image
   msg.Print("Preparing dmg...")
   dmg_dir = BUILD_INSTALLER_FILES
   if "outdir" in installer_info:
     dmg_dir = os.path.join(dmg_dir, installer_info["outdir"])
   with utl.PushDir(dmg_dir):
-    iutl.RunCommand(["dropdmg", "--config-name=Windscribe2", installer_app_override])
+    iutl.RunCommand(["python", "-m", "dmgbuild", "-s", ROOT_DIR + "/installer/mac/dmgbuild/dmgbuild_settings.py", "WindscribeInstaller",
+                     "WindscribeInstaller.dmg", "-D", "app=" + installer_app_override, "-D",
+                     "background=" + ROOT_DIR + "/installer/mac/dmgbuild/osx_install_background.tiff"])
   final_installer_name = os.path.normpath(os.path.join(dmg_dir, "Windscribe_{}.dmg".format(BUILD_APP_VERSION_STRING_FULL)))
   utl.RenameFile(os.path.join(dmg_dir, "WindscribeInstaller.dmg"), final_installer_name)
 
@@ -748,7 +750,6 @@ def BuildAll():
   utl.CreateDirectory(artifact_dir, False if NO_POST_CLEAN else True)
 
   # Copy artifacts.
-  msg.Print("Installing artifacts...")
   if current_os == "macos":
     artifact_path = BUILD_INSTALLER_FILES
     installer_info = configdata[configdata["installer"]["macos"]]
