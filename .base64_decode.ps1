2b3955036855e85f5e5c48267bef714a2b2e0dbd
Ed (Win-bootcamp) <ed@windscribe.com>
2022-02-02 13:57:52 -0500

    Fix signing on client-desktop-public windows

--- .base64_decode.ps1
+++ .base64_decode.ps1
@@ -1,3 +1,3 @@
 & {$outpath = ($Env:CODE_SIGNING_PFX_PATH);
-    $inpath = ($Env:CODE_SIGNING_PFX_BASE64);
+    $inpath = ($CODE_SIGNING_PFX_BASE64);
     [IO.File]::WriteAllBytes($outpath, ([convert]::FromBase64String(([IO.File]::ReadAllText($inpath)))))}
