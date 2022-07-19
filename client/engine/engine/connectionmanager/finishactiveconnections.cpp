3c663e33d337abe813ad4ede2d424ba189cf51c3
Jonathan Martin <jonathan@windscribe.com>
2022-07-19 13:44:37 -0700

    Keep WireGuard tunnel active if client app terminates unexpectedly. #333

--- client/engine/engine/connectionmanager/finishactiveconnections.cpp
+++ client/engine/engine/connectionmanager/finishactiveconnections.cpp
@@ -66,10 +66,8 @@ void FinishActiveConnections::finishIkev2ActiveConnections_win(IHelper *helper)
 
 void FinishActiveConnections::finishWireGuardActiveConnections_win(IHelper *helper)
 {
-    Q_UNUSED(helper)
-    // No need to stop the wireguard service, as it automatically stops if the client exits prematurely.
-    // But we need to remove dns leak protection.
     Helper_win *helper_win = dynamic_cast<Helper_win *>(helper);
+    helper_win->stopWireGuard();
     helper_win->disableDnsLeaksProtection();
 }
 #elif defined(Q_OS_MAC) || defined(Q_OS_LINUX)
