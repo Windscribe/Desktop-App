9ec1a608053d2e5c72156c27e7744c729380856a
Jason Xu <jason@windscribe.com>
2022-09-12 10:11:10 -0400

    Only evaluate WS_ASSERT on beta or guinea pig builds. #445

--- common/utils/ws_assert.h
+++ common/utils/ws_assert.h
@@ -3,11 +3,15 @@
 
 #include "logger.h"
 
+#if defined(WINDSCRIBE_IS_BETA) || defined(WINDSCRIBE_IS_GUINEA_PIG)
 #define WS_ASSERT(b) { \
   if (!(b)) { \
       qCDebug(LOG_ASSERT) << "Assertion failed! (" << __FILE__ << ":" << __LINE__ << ")"; \
   } \
   Q_ASSERT(b); \
 }
+#else
+#define WS_ASSERT(b)
+#endif
 
 #endif //WS_ASSERT_H
