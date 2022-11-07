f9572a1bbebc6b6a5ec77e3ad84b862d4c492046
Jonathan Martin <jonathan@windscribe.com>
2022-11-07 09:22:02 -0800

    Bump version number and update change log.

--- client/common/version/windscribe_version.h
+++ client/common/version/windscribe_version.h
@@ -3,11 +3,11 @@
 
 #define WINDSCRIBE_MAJOR_VERSION 2
 #define WINDSCRIBE_MINOR_VERSION 5
-#define WINDSCRIBE_BUILD_VERSION 10
+#define WINDSCRIBE_BUILD_VERSION 11
 
 // only one of these should be enabled; neither -> stable
 //#define WINDSCRIBE_IS_BETA
-#define WINDSCRIBE_IS_GUINEA_PIG
+//#define WINDSCRIBE_IS_GUINEA_PIG
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
