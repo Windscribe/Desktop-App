51115fb672deaa6e1b01c706aabfda0b6634b87f
Alexey Azanov <alexey@windscribe.com>
2023-01-21 04:24:45 +0700

    Added DgaFailover class. #325

---
+++ client/engine/engine/failover/failovers/dgafailover.cpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "dgafailover.h"
+
+#include <QCoreApplication>
+#include <QLibrary>
+
+#include "utils/logger.h"
+#include "utils/executable_signature/executable_signature.h"
+
+
+namespace failover {
+
+void DgaFailover::getHostnames(bool)
+{
+    QString dgaLibPath = QCoreApplication::applicationDirPath() + "/dga_library.dll";
+
+    if (!QFile::exists(dgaLibPath)) {
+        qCDebug(LOG_BASIC) << "No dga, skip it";
+        emit finished(FailoverRetCode::kFailed, QStringList());
+        return;
+    }
+
+    ExecutableSignature sigCheck;
+    if (!sigCheck.verifyWithSignCheck(dgaLibPath.toStdWString()))
+    {
+        qCDebug(LOG_BASIC) << "Failed to verify dga signature: " << QString::fromStdString(sigCheck.lastError());
+        emit finished(FailoverRetCode::kFailed, QStringList());
+        return;
+    }
+
+    QLibrary lib(dgaLibPath);
+    if (!lib.load()) {
+        emit finished(FailoverRetCode::kFailed, QStringList());
+        return;
+    }
+
+    typedef void (*FuncPrototype)(char *outStr, int length);
+    FuncPrototype func = (FuncPrototype) lib.resolve("d1");
+    char buf[1024] = {0};
+    if (func) {
+        func(buf, 1024);
+    }
+    lib.unload();
+
+    QString domain = QString::fromStdString(buf);
+    if (!domain.isEmpty())
+    {
+        qCDebug(LOG_FAILOVER) << "DGA domain:" << domain;      // TODO: remove
+        emit finished(FailoverRetCode::kSuccess, QStringList() << domain);
+    }
+    else
+        emit finished(FailoverRetCode::kFailed, QStringList());
+}
+
+} // namespace failover
