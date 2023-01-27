dd31ea7eb074f42788e31e3226a45b3d9cbdf3dd
Milan Hauth <milahu@gmail.com>
2023-01-27 18:32:34 +0100

    Revert "qt6: fix error: variable QRegularExpression has initializer but incomplete type"
    This reverts commit deecc43936fc82551a8737a938dce9d283879dc6.

--- client/gui/connectwindow/ipaddressitem/ipaddressitem.cpp
+++ client/gui/connectwindow/ipaddressitem/ipaddressitem.cpp
@@ -63,10 +63,7 @@ void IPAddressItem::setIpAddress(const QString &ip, bool bWithAnimation)
         bool prevIsValid = isValid_;
 
         QString ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
-
-        QString ipRegexString ("^" + ipRange + "\\." + ipRange + "\\." + ipRange + "\\." + ipRange + "$");
-        QRegularExpression ipRegex (ipRegexString);
-
+        QRegularExpression ipRegex ("^" + ipRange + "\\." + ipRange + "\\." + ipRange + "\\." + ipRange + "$");
         isValid_ = ipRegex.match(ip).hasMatch();
 
         if (isValid_)
