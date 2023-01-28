aa4bfe6129d898ac4df6739de9f3e4866548ae6f
Milan Hauth <milahu@gmail.com>
2023-01-28 21:09:25 +0100

    cmake

--- tools/build.sh
+++ tools/build.sh
@@ -4,17 +4,17 @@ set -e
 set -x
 
 mkdir build || true
-cd build
 
 # gnumake
 #cmake ..
 #make
 
 # ninja
-cmake -G "Ninja" ..
+cmake -G "Ninja" -B build
 TERM=dumb # disable line-clearing in ninja
 ninjaFlags=()
 #ninjaFlags+=(-v) # verbose: show commands
 #ninjaFlags+=(-d explain) # debug
 ninjaFlags+=(-j1) # single threaded
+ninjaFlags+=(-C build)
 ninja "${ninjaFlags[@]}"
