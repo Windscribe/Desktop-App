cf0792151bf28735e88819755e388e415c6235a5
Jonathan Martin <jonathan@windscribe.com>
2023-05-19 07:53:59 -0700

    Add Windows arm64 support to install_wireguard script. #592

--- tools/deps/custom_wireguard/embeddable-dll-service/build.bat
+++ tools/deps/custom_wireguard/embeddable-dll-service/build.bat
@@ -10,34 +10,35 @@ cd /d %BUILDDIR% || exit /b 1
 
 if exist ..\.deps\prepared goto :build
 :installdeps
-	call ..\build.bat || goto :error
+    call ..\build.bat || goto :error
 
 :build
-	set GOOS=windows
-	set GOARM=7
-	set GOPATH=%BUILDDIR%..\.deps\gopath
-	set GOROOT=%BUILDDIR%..\.deps\go
-	set CGO_ENABLED=1
-	set CGO_CFLAGS=-O3 -Wall -Wno-unused-function -Wno-switch -std=gnu11 -DWINVER=0x0601
-	rem call :build_plat x86 i686 386 || goto :error
-	call :build_plat amd64 x86_64 amd64 || goto :error
-	rem call :build_plat arm64 aarch64 arm64 || goto :error
+    set GOOS=windows
+    set GOARM=7
+    set GOPATH=%BUILDDIR%..\.deps\gopath
+    set GOROOT=%BUILDDIR%..\.deps\go
+    set CGO_ENABLED=1
+    set CGO_CFLAGS=-O3 -Wall -Wno-unused-function -Wno-switch -std=gnu11 -DWINVER=0x0601
+    rem call :build_plat x86 i686 386 || goto :error
+    call :build_plat amd64 x86_64 amd64 || goto :error
+    call :build_plat arm64 aarch64 arm64 || goto :error
 
 :success
-	echo [+] Success
-	exit /b 0
+    echo [+] Success
+    exit /b 0
 
 :build_plat
-	set CC=%~2-w64-mingw32-gcc
-	set GOARCH=%~3
-	mkdir %1 >NUL 2>&1
+    set CC=%~2-w64-mingw32-gcc
+    set WINDRES=%~2-w64-mingw32-windres.exe
+    set GOARCH=%~3
+    mkdir %1 >NUL 2>&1
     echo [+] Building version info
-    windres.exe -i version_info.rc -O coff -o version_info.syso
-	echo [+] Building library %1
-	go build -buildmode c-shared -ldflags="-w -s" -trimpath -v -o "%~1/tunnel.dll" || exit /b 1
-	del "%~1\tunnel.h"
-	goto :eof
+    %WINDRES% -i version_info.rc -O coff -o version_info.syso
+    echo [+] Building library %1
+    go build -buildmode c-shared -ldflags="-w -s" -trimpath -v -o "%~1/tunnel.dll" || exit /b 1
+    del "%~1\tunnel.h"
+    goto :eof
 
 :error
-	echo [-] Failed with error #%errorlevel%.
-	cmd /c exit %errorlevel%
+    echo [-] Failed with error #%errorlevel%.
+    cmd /c exit %errorlevel%
