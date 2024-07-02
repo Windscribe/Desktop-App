1b5cbb797bc46f64d5a24efa7f346fe30da43f3f
Jonathan Martin <bernerdad@users.noreply.github.com>
2024-07-02 09:23:02 -0700

    v2.10.15

--- client/common/version/windscribe_version.h
+++ client/common/version/windscribe_version.h
@@ -2,10 +2,10 @@
 
 #define WINDSCRIBE_MAJOR_VERSION 2
 #define WINDSCRIBE_MINOR_VERSION 10
-#define WINDSCRIBE_BUILD_VERSION 14
+#define WINDSCRIBE_BUILD_VERSION 15
 
 // only one of these should be enabled; neither -> stable
-#define WINDSCRIBE_IS_BETA
+//#define WINDSCRIBE_IS_BETA
 //#define WINDSCRIBE_IS_GUINEA_PIG
 
 #define STR_HELPER(x) #x
