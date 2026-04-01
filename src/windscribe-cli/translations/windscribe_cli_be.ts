dd23ba38d57b9a9d167749d3f92e472e15653e95
dubovy-achvelak <268134219+dubovy-achvelak@users.noreply.github.com>
2026-04-01 18:22:52 +0300

    Translate to Belarusian (#308)
    * Capitalize Belarusian language name for consistency with other languages
    
    * Add Belarusian language to language switcher
    
    * Add translation of main app, CLI, installer, uninstaller to Belarusian language
    
    * Include Belarusian translation of main app, CLI, intaller, uninstaller to CMake and Windows resources

---
+++ src/windscribe-cli/translations/windscribe_cli_be.ts
@@ -0,0 +1,338 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be_BY">
+<context>
+    <name>BackendCommander</name>
+    <message>
+        <source>Data usage: %1 / %2</source>
+        <translation>Выкарыстаньне трафіку: %1 / %2</translation>
+    </message>
+    <message>
+        <source>Unlimited</source>
+        <translation>Безьліміт</translation>
+    </message>
+    <message>
+        <source>No locations.</source>
+        <translation>Няма месцазнаходжаньняў.</translation>
+    </message>
+    <message>
+        <source>Could not rotate IP.  Please check that you have Windscribe Pro or have this location in your plan, or try again later.</source>
+        <translation>Не ўдалося зьмяніць IP-адрас.  Праверце, ці маеце Windscribe Pro, альбо ці ўваходзіць гэтае месцазнаходжаньне ў вашы тарыфны плян, альбо паўтарыце спробу пазьней.</translation>
+    </message>
+    <message>
+        <source>Could not favourite IP.  Please check that you have Windscribe Pro or have this location in your plan, or try again later.</source>
+        <translation>Не ўдалося дадаць IP-адрас у абранае.  Праверце, ці маеце Windscribe Pro, альбо ці ўваходзіць гэтае месцазнаходжаньне ў вашы тарыфны плян, альбо паўтарыце спробу пазьней.</translation>
+    </message>
+    <message>
+        <source>Could not unfavourite IP.  Please check that the provided IP is valid.</source>
+        <translation>Не ўдалося прыбраць IP-адрас з абранага.  Праверце, ці правільны ўведзены IP-адрас.</translation>
+    </message>
+    <message>
+        <source>IP rotate already in progress.</source>
+        <translation>Зьмена IP-адраса ўжо ідзе.</translation>
+    </message>
+    <message>
+        <source>No AmneziaWG configurations available.</source>
+        <translation>Няма даступных канфіґурацый AmneziaWG.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Aborting: app did not start in time</source>
+        <translation>Перапыненьне: праґрама не запусцілася ў адведзены час</translation>
+    </message>
+    <message>
+        <source>Aborting: IPC communication error</source>
+        <translation>Перапыненьне: памылка межпрацэснай сувязі (IPC)</translation>
+    </message>
+    <message>
+        <source>Not logged in</source>
+        <translation>Уваход ва ўліковы запіс не выкананы</translation>
+    </message>
+    <message>
+        <source>Firewall already on</source>
+        <translation>Брандмаўэр ужо ўключаны</translation>
+    </message>
+    <message>
+        <source>Firewall already off</source>
+        <translation>Брандмаўэр ужо выключаны</translation>
+    </message>
+    <message>
+        <source>Firewall set to always on and can&apos;t be turned off</source>
+        <translation>Брандмаўэр заўсёды уключаны і не можа быць выключаны</translation>
+    </message>
+    <message>
+        <source>Already logged in</source>
+        <translation>Ужо ўвайшлі ва ўліковы запіс</translation>
+    </message>
+    <message>
+        <source>Already logged out</source>
+        <translation>Ужо выйшлі з уліковага запісу</translation>
+    </message>
+    <message>
+        <source>No update available</source>
+        <translation>Абнаўленьняў няма</translation>
+    </message>
+    <message>
+        <source>Internet connectivity: %1</source>
+        <translation>Падлучэньне да Інтэрнэту: %1</translation>
+    </message>
+    <message>
+        <source>Login state: %1</source>
+        <translation>Стан уваходу: %1</translation>
+    </message>
+    <message>
+        <source>Logged out</source>
+        <translation>Выйшлі</translation>
+    </message>
+    <message>
+        <source>Logging in</source>
+        <translation>Уваход ідзе</translation>
+    </message>
+    <message>
+        <source>Logged in</source>
+        <translation>Увайшлі</translation>
+    </message>
+    <message>
+        <source>Error: %1</source>
+        <translation>Памылка: %1</translation>
+    </message>
+    <message>
+        <source>No internet connectivity</source>
+        <translation>Адсутнічае падлучэньне да Інтэрнэту</translation>
+    </message>
+    <message>
+        <source>No API connectivity</source>
+        <translation>Адсутнічае падлучэньне да API</translation>
+    </message>
+    <message>
+        <source>Incorrect username, password, or 2FA code</source>
+        <translation>Няправільныя імя карыстальніка, пароль альбо код 2FA</translation>
+    </message>
+    <message>
+        <source>SSL error</source>
+        <translation>Памылка SSL</translation>
+    </message>
+    <message>
+        <source>Session expired</source>
+        <translation>Сесія скончаная</translation>
+    </message>
+    <message>
+        <source>Rate limited</source>
+        <translation>Доступ абмежаваны</translation>
+    </message>
+    <message>
+        <source>Unknown error</source>
+        <translation>Невядомая памылка</translation>
+    </message>
+    <message>
+        <source>Connect state: %1</source>
+        <translation>Стан злучэньня: %1</translation>
+    </message>
+    <message>
+        <source>Connected: %1</source>
+        <translation>Падлучана: %1</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>Падлучана</translation>
+    </message>
+    <message>
+        <source>[Network interference]</source>
+        <translation>[Перашкоды ў сетцы]</translation>
+    </message>
+    <message>
+        <source>Connecting: %1</source>
+        <translation>Ідзе падлучэньне: %1</translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation>Ідзе падлучэньне</translation>
+    </message>
+    <message>
+        <source>Disconnecting</source>
+        <translation>Ідзе адлучэньне</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>Адлучана</translation>
+    </message>
+    <message>
+        <source>Error: Location does not exist or is disabled</source>
+        <translation>Памылка: месцазнаходжаньне не існуе альбо адключана</translation>
+    </message>
+    <message>
+        <source>Error: You are out of data, or your account has been disabled. Upgrade to Pro to continue using Windscribe</source>
+        <translation>Памылка: у вас скончыўся трафік альбо ўліковы запіс заблякаваны. Перайдзіце на Pro, каб працягваць карыстацца Windscribe</translation>
+    </message>
+    <message>
+        <source>Error: Unable to start custom DNS service</source>
+        <translation>Памылка: не ўдалося запусьціць уласную службу DNS</translation>
+    </message>
+    <message>
+        <source>Error: WireGuard adapter setup failed</source>
+        <translation>Памылка: не ўдалося наладзіць адаптар WireGuard</translation>
+    </message>
+    <message>
+        <source>Error: Could not retrieve WireGuard configuration</source>
+        <translation>Памылка: не ўдалося атрымаць канфіґурацыю WireGuard</translation>
+    </message>
+    <message>
+        <source>Unknown state</source>
+        <translation>Невядомы стан</translation>
+    </message>
+    <message>
+        <source>Protocol: %1:%2</source>
+        <translation>Пратакол: %1:%2</translation>
+    </message>
+    <message>
+        <source>Firewall state: %1</source>
+        <translation>Стан брандмаўэра: %1</translation>
+    </message>
+    <message>
+        <source>Always On</source>
+        <translation>Заўсёды ўключаны</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>Уключаны</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Выключаны</translation>
+    </message>
+    <message>
+        <source>Update error: download failed. Please try again or try a different network.</source>
+        <translation>Памылка абнаўленьня: не ўдалося спампаваць. Паўтарыце спробу, альбо падлучыцеся да іншай сеткі.</translation>
+    </message>
+    <message>
+        <source>Update error: %1</source>
+        <translation>Памылка абнаўленьня: %1</translation>
+    </message>
+    <message>
+        <source>Update available: %1</source>
+        <translation>Даступнае абнаўленьне: %1</translation>
+    </message>
+    <message>
+        <source>available</source>
+        <translation>даступнае</translation>
+    </message>
+    <message>
+        <source>unavailable</source>
+        <translation>недаступнае</translation>
+    </message>
+    <message>
+        <source>Disconnected due to reaching WireGuard key limit.  Use &quot;windscribe-cli keylimit delete&quot; if you want to delete the oldest key instead, and try again.</source>
+        <translation>Адлучана: дасягнуты ліміт ключоў WireGuard.  Выкарыстоўвайце &quot;windscribe-cli keylimit delete&quot;, каб выдаліць найстарэйшы ключ, і паспрабуйце яшчэ раз.</translation>
+    </message>
+    <message>
+        <source>Downloading: %1%</source>
+        <translation>Ідзе спампоўваньне: %1%</translation>
+    </message>
+    <message>
+        <source>Username too short</source>
+        <translation>Імя карыстальніка занадта кароткае</translation>
+    </message>
+    <message>
+        <source>Password too short</source>
+        <translation>Пароль занадта кароткі</translation>
+    </message>
+    <message>
+        <source>Connection is in progress.  Use &apos;windscribe-cli status&apos; to check for connection status.</source>
+        <translation>Ідзе падлучэньне.  Выкарыстоўвайце &apos;windscribe-cli status&apos;, каб праверыць стан злучэньня.</translation>
+    </message>
+    <message>
+        <source>Disconnection is in progress.  Use &apos;windscribe-cli status&apos; to check for connection status.</source>
+        <translation>Ідзе адлучэньне.  Выкарыстоўвайце &apos;windscribe-cli status&apos;, каб праверыць стан злучэньня.</translation>
+    </message>
+    <message>
+        <source>Firewall is on.</source>
+        <translation>Брандмаўэр уключаны.</translation>
+    </message>
+    <message>
+        <source>Firewall is off.</source>
+        <translation>Брандмаўэр выключаны.</translation>
+    </message>
+    <message>
+        <source>Logs sent.</source>
+        <translation>Журналы адасланыя.</translation>
+    </message>
+    <message>
+        <source>Preferences reloaded.</source>
+        <translation>Налады перазагружаныя.</translation>
+    </message>
+    <message>
+        <source>Key limit behaviour is set.</source>
+        <translation>Паводзіны пры дасягненьні ліміту ключоў зададзеныя.</translation>
+    </message>
+    <message>
+        <source>Connection has been overridden by another command.</source>
+        <translation>Падлучэньне было перавызначана іншай камандай.</translation>
+    </message>
+    <message>
+        <source>Disconnection has been overridden by another command.</source>
+        <translation>Адлучэньне было перавызначана іншай камандай.</translation>
+    </message>
+    <message>
+        <source>Already disconnected</source>
+        <translation>Ужо адключана</translation>
+    </message>
+    <message>
+        <source>Logging out</source>
+        <translation>Выхад ідзе</translation>
+    </message>
+    <message>
+        <source>Need 2FA code</source>
+        <translation>Патрэбны код 2FA</translation>
+    </message>
+    <message>
+        <source>Incorrect 2FA code</source>
+        <translation>Некарэктны код 2FA</translation>
+    </message>
+    <message>
+        <source>(Device name: %1)</source>
+        <translation>(Імя прылады: %1)</translation>
+    </message>
+    <message>
+        <source>Internet connectivity is not available.  Try again later.</source>
+        <translation>Падлучэньне да Інтэрнэту недаступнае.  Паўтарыце спробу пазьней.</translation>
+    </message>
+    <message>
+        <source>Please type the numbers above to continue:</source>
+        <translation>Каб працягнуць, увядзіце лічбы вышэй:</translation>
+    </message>
+    <message>
+        <source> (10 Gbps)</source>
+        <translation> (10 Гбіт/с)</translation>
+    </message>
+    <message>
+        <source> (Disabled)</source>
+        <translation> (Адключанае)</translation>
+    </message>
+    <message>
+        <source> (Pro)</source>
+        <translation> (Pro)</translation>
+    </message>
+    <message>
+        <source>Not connected</source>
+        <translation>Не падлучана</translation>
+    </message>
+    <message>
+        <source>IP rotated.</source>
+        <translation>IP-адрас зьменены.</translation>
+    </message>
+    <message>
+        <source>IP favorited.</source>
+        <translation>IP-адрас дададзены ў абранае.</translation>
+    </message>
+    <message>
+        <source>IP unfavorited.</source>
+        <translation>IP-адрас прыбраны з абранага.</translation>
+    </message>
+    <message>
+        <source>Invalid IP address</source>
+        <translation>Некарэктны IP-адрас</translation>
+    </message>
+</context>
+</TS>
